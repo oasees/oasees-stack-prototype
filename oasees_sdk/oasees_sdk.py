c94a50a4092b2e750fc12468c237d2186a81db4b
Achilleas Economopoulos <achilleasecon@gmail.com>
2024-01-23 15:12:02 +0200

    v 0.1.0

--- oasees_sdk/oasees_sdk.py
+++ oasees_sdk/oasees_sdk.py
@@ -7,49 +7,47 @@ import requests
 import os
 
 
-env_file_path = '../.env'  # Replace this with the actual path to your .env file
-load_dotenv(dotenv_path=env_file_path)
-IPFS_HOST = os.getenv('IPFS_HOST')
-BLOCK_CHAIN_IP = os.getenv('BLOCK_CHAIN_IP')
-DAO_STORAGE_ADDRESS = os.getenv('DAO_STORAGE_ADDRESS')
-ACCOUNT_ADDRESS = os.getenv('ACCOUNT_ADDRESS')
-ACCOUNT_ADDRESS = Web3.to_checksum_address(str(ACCOUNT_ADDRESS))
+
+load_dotenv()
+__IPFS_HOST = os.getenv('IPFS_HOST')
+__BLOCK_CHAIN_IP = os.getenv('BLOCK_CHAIN_IP')
+__ACCOUNT_ADDRESS = os.getenv('ACCOUNT_ADDRESS')
+__ACCOUNT_ADDRESS = Web3.to_checksum_address(str(__ACCOUNT_ADDRESS))
+
 
 ###### INITIALIZE THE CONNECTIONS TO THE SERVICES AND CONTRACTS INVOLVED ######
 
-web3 = Web3(Web3.HTTPProvider(f"http://{BLOCK_CHAIN_IP}:8545"))                    # BLOCKCHAIN
-response = requests.get(f'http://{IPFS_HOST}:6001/ipfs_portal_contracts')
-data = response.json()
-ipfs_json = data['portal_contracts']
+__web3 = Web3(Web3.HTTPProvider(f"http://{__BLOCK_CHAIN_IP}:8545"))                    # BLOCKCHAIN
+__response = requests.get(f'http://{__IPFS_HOST}:6001/ipfs_portal_contracts')
+__data = __response.json()
+__ipfs_json = __data['portal_contracts']
 
 
-nft_abi = ipfs_json['nft_abi']             
-nft_address = ipfs_json['nft_address']
-marketplace_abi = ipfs_json['marketplace_abi']
-marketplace_address = ipfs_json['marketplace_address']
-daoStorage_abi = ipfs_json['daoStorage_abi']
+__nft_abi = __ipfs_json['nft_abi']             
+__nft_address = __ipfs_json['nft_address']
+__marketplace_abi = __ipfs_json['marketplace_abi']
+__marketplace_address = __ipfs_json['marketplace_address']
 
 
-nft = web3.eth.contract(address=nft_address, abi=nft_abi)                           # NFT contract
-nft_marketplace = web3.eth.contract(address=marketplace_address, 
-                                    abi=marketplace_abi)                            # Marketplace contract
-
-
+__nft = __web3.eth.contract(address=__nft_address, abi=__nft_abi)                           # NFT contract
+__marketplace = __web3.eth.contract(address=__marketplace_address, 
+                                    abi=__marketplace_abi)                            # Marketplace contract
 
 
 
-def getPurchases():
-    # Retrieves the account's purchases and returns them in a list
-    if (ACCOUNT_ADDRESS):
 
-        client = ipfshttpclient.connect(f"/ip4/{IPFS_HOST}/tcp/5001")                       # IPFS
 
-        results = nft_marketplace.caller({'from': ACCOUNT_ADDRESS}).getMyNfts()
+def __getPurchases():
+    if (__ACCOUNT_ADDRESS):
+
+        client = ipfshttpclient.connect(f"/ip4/{__IPFS_HOST}/tcp/5001")                       # IPFS
+
+        results = __marketplace.caller({'from': __ACCOUNT_ADDRESS}).getMyNfts()
         purchases=[]
 
         for r in results:
             token_id = r[1]
-            content_hash = nft.functions.tokenURI(token_id).call()
+            content_hash = __nft.functions.tokenURI(token_id).call()
             metadata_hash = r[5]
 
             
@@ -62,81 +60,162 @@ def getPurchases():
 
         client.close()
 
-        
         return purchases
 
 
+def __getDevices():
+    results = __marketplace.caller({'from': __ACCOUNT_ADDRESS}).getMyDevices()
+    devices = []
+
+    client = ipfshttpclient.connect(f"/ip4/{__IPFS_HOST}/tcp/5001")  
+
+    for r in results:
+        token_id = r[1]
+        content_hash = __nft.functions.tokenURI(token_id).call()
+
+        content = client.cat(content_hash)
+        content = content.decode("UTF-8")
+        content = json.loads(content)
+        
+        devices.append({'name': content['name'], 'endpoint':content['device_endpoint'][7:]})
+    
+    client.close()
+    
+    return devices
 
 
-def list_items():
 
-    purchases = getPurchases()
+def my_algorithms():
+    '''Returns a list with all the algorithms purchased from your account
+        on the OASEES Marketplace.''' 
 
+    purchases = __getPurchases()
+
+
+    print("\nOwned algorithms")
+    print("---------------------------------")
+    i=1
     if(purchases):
         for purchase in purchases:
-            print(purchase['title'])
+            print(str(i) + ") " + purchase['title'])
+            i+=1
     
     else:
-        print("You have not bought any items from the marketplace yet.")
+        print("You have not bought any items from the __marketplace yet.")
 
 
 
-def register_devices():
-    # Retrieves the names and URLs of all the devices participating in the user's joined DAOs
-    daoStorage_contract = web3.eth.contract(address=DAO_STORAGE_ADDRESS, abi=daoStorage_abi)
-    daos_joined = daoStorage_contract.functions.getStoredHashes().call()
+def my_devices():
+    '''Returns a list with all the devices purchased / uploaded from your account
+        on the OASEES Marketplace.''' 
 
+    devices = __getDevices()
 
-    if(daos_joined):
-        client = ipfshttpclient.connect(f"/ip4/{IPFS_HOST}/tcp/5001")                       # IPFS
-        
-        for dao_hash in daos_joined:
-            dao_info = client.cat(dao_hash)
-            dao_info = dao_info.decode("UTF-8")
-            dao_info = json.loads(dao_info)
-
-            print("Devices participating in " + dao_info['dao_name'] + ":")
-            for device in dao_info['devices']:
-                status = ''
-                try:
-                    response = requests.get('{}/register'.format(device['endpoint']))
-                    status = "ONLINE"
-                except requests.exceptions.RequestException:
-                    status = "OFFLINE"
-
-                print(device['device_name'], device['endpoint'][7:],status)
-                    
-        client.close()
-
+    print("\nOwned devices")
+    print("---------------------------------")
+    i=1
+    if(devices):
+        for device in devices:
+            print(str(i) + ") " + device['name'] + " | " + device['endpoint'])
+            i+=1
+    
     else:
-        print("You have not joined any DAOs yet.")
+        print("You have not bought any items from the __marketplace yet.")
 
 
-def deploy(algo_title:str):
-    daoStorage_contract = web3.eth.contract(address=DAO_STORAGE_ADDRESS, abi=daoStorage_abi)
-    daos_joined = daoStorage_contract.functions.getStoredHashes().call()
 
-    purchases = getPurchases()
+
+def deploy_algorithm(algorithm_title:str):
+    '''Deploys a purchased algorithm on all your connected devices.
+
+        - algorithm_title: Needs to be provided in "string" form.
+    
+        e.g. algorithm.py -> deploy_algorithm("algorithm.py")
+    '''
+
+    purchases = __getPurchases()
+    devices = __getDevices()
     found = False
     for purchase in purchases:
         if found:
             break
 
-        if(purchase['title']==algo_title):
+        if(purchase['title']==algorithm_title):
             found = True
             algo_hash = purchase['contentURI']
 
-            client = ipfshttpclient.connect(f"/ip4/{IPFS_HOST}/tcp/5001")                       # IPFS
+            client = ipfshttpclient.connect(f"/ip4/{__IPFS_HOST}/tcp/5001")                       # IPFS
 
-            for dao_hash in daos_joined:
-                dao_info = client.cat(dao_hash)
-                dao_info = dao_info.decode("UTF-8")
-                dao_info = json.loads(dao_info)
-                for device in dao_info['devices']:
-                    response = requests.post("{}/deploy_algorithm".format(device['endpoint']), json = {'algorithm_hash': algo_hash, 'algorithm_name':algo_title})
-
+            for device in devices:
+                __response = requests.post("http://{}/deploy_algorithm".format(device['endpoint']), json = {'algorithm_hash': algo_hash, 'algorithm_name':algorithm_title})                 
+                print(__response.text)
             client.close()
             
 
     if not found:
         print("The file you requested was not found in your purchases.")
+
+
+
+def deploy_local_file(path:str):
+    '''Deploys the file found in the specified path on all your connected devices.
+    
+        - path: -> Needs to be provided in "string" form.
+                -> Is equal to the filename when the file is located in
+                   the Jupyter Notebook's directory.
+    
+        e.g. algorithm.py -> deploy_local_file("algorithm.py")
+    '''
+
+    devices = __getDevices()
+    file = open(path,"rb")
+
+    for device in devices:
+        __response= requests.post("http://{}/deploy_file".format(device['endpoint']), files={'file': file})                 
+        print(__response.text)
+    
+    file.close()
+    
+
+
+def instructions():
+    text = (f'''
+    \033[1m{my_algorithms.__name__}\033[0m() \t\t
+        {my_algorithms.__doc__}
+
+
+    \033[1m{my_devices.__name__}\033[0m() \t   \t
+        {my_devices.__doc__}
+    
+
+    \033[1m{deploy_algorithm.__name__}\033[0m(algorithm_title: str) \t \t
+        {deploy_algorithm.__doc__}
+
+
+    \033[1m{deploy_local_file.__name__}\033[0m(path: str) \t   \t
+        {deploy_local_file.__doc__}
+
+        
+    \033[1m{instructions.__name__}\033[0m() \t \t
+        Reprints the above documentation.
+        ''')
+    
+    __print_msg_box(text,title="\033[1mOASEES SDK methods\033[0m \t \t")
+
+ 
+def __print_msg_box(msg, indent=1, width=None, title=None):
+    """Print message-box with optional title."""
+    lines = msg.split('\n')
+    space = " " * indent
+    if not width:
+        width = max(map(len, lines))
+    box = f'╔{"═" * (width + indent * 2)}╗\n'  # upper_border
+    if title:
+        box += f'║{space}{title:<{width}}{space}║\n'  # title
+        box += f'║{space}{"-" * len(title):<{width}}{space}║\n'  # underscore
+    box += ''.join([f'║{space}{line:<{width}}{space}║\n' for line in lines])
+    box += f'╚{"═" * (width + indent * 2)}╝'  # lower_border
+    print(box)
+
+
+instructions()
