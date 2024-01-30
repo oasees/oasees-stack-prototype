2aade99eac1b98c22dd1b824ad79c7cd37a51949
Achilleas Economopoulos <achilleasecon@gmail.com>
2024-01-30 12:26:26 +0200

    New DApp page.

---
+++ portal/src/components/dapp-page/DApp.tsx
@@ -0,0 +1,93 @@
+import { Button, Center, CloseButton, Flex, Table } from "@mantine/core";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface DAppHTML{
+    html_page: string;
+    closeFunction(): void;
+    device_endpoint: string;
+}
+
+
+const DApp = ({html_page, closeFunction,device_endpoint}:DAppHTML) => {
+
+    const [wavFiles, setWavFiles] = useState<string[]>([])
+    const [fileToPlay, setFileToPlay] = useState(-1);
+
+    // const handleButtonClick = () => {
+    //     // Add your custom logic here
+    //     const button = document.getElementById('play');
+    
+    //     if (button) {
+    //       button.addEventListener('click', async () => {
+    //         console.log(fileToPlay);
+    //         console.log(wavFiles[fileToPlay])
+    //         await axios.post(`${device_endpoint}/play`,wavFiles[fileToPlay]);
+    //       });
+    //     }
+    //   };
+
+    // const handleButtonClick = async () => {
+    //     console.log(wavFiles[fileToPlay]);
+    //     await axios.post(`${device_endpoint}/play`,{audio_file:wavFiles[fileToPlay]});
+    // }
+
+    const playFile = async (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+        const button:HTMLButtonElement = event.currentTarget;
+
+        await axios.post(`${device_endpoint}/play`,{audio_file:wavFiles[Number(button.value)]});
+    }
+
+    useEffect(()=>{
+        const getAudioFiles = async() => {
+            const audio_files:any = await axios.get(`${device_endpoint}/list_wav_files`);
+            setWavFiles(audio_files.data.wav_files);
+        }
+
+        getAudioFiles();
+        // handleButtonClick();
+    },[]);
+
+    const mapped_files = () => {
+        if(wavFiles){
+            return (
+                wavFiles.map((file,index) => (
+                <Table.Tr key={index}>
+                    <Table.Td>{file}</Table.Td>
+                    <Table.Td><Button color='green' onClick={playFile} value={index}>Play</Button></Table.Td>
+                </Table.Tr>
+            )));
+        }
+    };
+
+    return (
+        <>
+        <Flex justify="end">
+            <CloseButton onClick={closeFunction}/>
+        </Flex>
+
+        <Center>
+            <h3>Sample DApp</h3>
+        </Center>
+        <Table striped={true} stripedColor="var(--mantine-color-gray-1)" withRowBorders withTableBorder>
+            <Table.Thead>
+                <Table.Tr>
+                    <Table.Th>Audio file</Table.Th>
+                </Table.Tr>
+            </Table.Thead>
+
+            <Table.Tbody>
+                {mapped_files()}
+            </Table.Tbody>
+        </Table>
+        <Flex justify='center'>
+            <Button color="red">Record</Button>
+        </Flex>
+        {/* <div dangerouslySetInnerHTML={{ __html: html_page }} /> */}
+        </>
+    );
+}
+
+
+export default DApp;
---
+++ portal/src/components/dapp-page/DAppContainer.tsx
@@ -0,0 +1,175 @@
+import React, { useEffect, useState } from 'react';
+import '@mantine/core/styles.css'
+import '@mantine/dropzone/styles.css';
+import { Button, Card, Group, SimpleGrid } from '@mantine/core';
+import axios from 'axios';
+import DApp from './DApp';
+
+
+async function hitDApp(action:string){
+
+    const config = {
+      headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+
+      },
+      data: {},
+      params: {
+      }
+    }
+
+    try{
+    const response = await axios.get(`http://10.160.1.225:8000/${action}`,config);
+
+    }catch(error){
+        console.error(error);
+    }
+    return 
+
+  }
+
+
+async function IpfsGet(_ipfs_hash:string){
+
+  const config = {
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+
+    },
+    data: {},
+    params: {
+      "ipfs_hash": _ipfs_hash
+    }
+  }
+
+  const response = await axios.get(`http://${process.env.REACT_APP_INFRA_HOST}/ipfs_fetch`,config);
+
+
+  return response.data.content;
+
+}
+
+async function IpfsGetDapp(_ipfs_hash:string){
+
+  const config = {
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+
+    },
+    data: {},
+    params: {
+      "ipfs_hash": _ipfs_hash
+    }
+  }
+
+  const response = await axios.get(`http://${process.env.REACT_APP_INFRA_HOST}/ipfs_fetch_dapp`,config);
+
+
+  return response.data.content;
+
+}
+
+interface DAppProps{
+  json:any
+}
+
+
+function DAppContainer({json}:DAppProps) {
+  const marketplaceMonitor = json.marketplace.connect(json.callProvider);
+
+  const [dapps, setDapps] = useState<string[]>([]);
+  const [activeDApp, setActiveDApp] = useState(-1);
+  const [dappInfos, setDappInfos] = useState<any[]>([]);
+
+  const [deviceEndpoint,setDeviceEndpoint] = useState('');
+
+  useEffect(()=>{
+    const getDAppHashes = async () => {
+      try{
+        let dapp_pages = [];
+        let dapp_infos = [];
+        const available_daos = await marketplaceMonitor.getJoinedDaos({from: json.account});
+        for (const dao of available_daos){
+          const dao_content_hash = await json.nft.tokenURI(dao[1]);
+          const content = await IpfsGet(dao_content_hash);
+          const name = content.dapp_info.NAME;
+          const description = content.dapp_info.DESCRIPTION;
+
+
+          const dapp_page = await IpfsGetDapp(content.dapp);
+
+          dapp_pages.push(dapp_page);
+          dapp_infos.push({name:name, description:description})
+        }
+
+        setDapps(dapp_pages);
+        setDappInfos(dapp_infos);
+
+      } catch(error){
+          console.error('Error loading contracts: ', error);
+      }
+    }
+
+    getDAppHashes();
+  },[])
+
+  useEffect(()=>{
+    const getDevice = async() => {
+      const available_devices = await marketplaceMonitor.getMyDevices({from: json.account});
+      const device = available_devices[0];
+
+      const content_hash = await json.nft.tokenURI(device[1]);
+      const content = await IpfsGet(content_hash);
+      setDeviceEndpoint(content.device_endpoint);
+    }
+
+    getDevice();
+  },[])
+
+  const play = () => {
+    hitDApp("play");
+  }
+
+  const record = () => {
+    hitDApp("record");
+  }
+
+  const dapp_cards = dapps.map((dapp,index)=>(
+    <Card shadow="sm" padding="lg" radius="md" withBorder ta="center" key={index}>
+      <h3>{dappInfos[index].name}</h3>
+      <h4>{dappInfos[index].description}</h4>
+      <Button color="orange" onClick={()=>setActiveDApp(index)}>Show DApp</Button>
+    </Card>
+  ))
+
+  const handleClose = () => {
+    setActiveDApp(-1);
+  }
+
+
+  const pageContent = () => {
+    if(activeDApp<0){
+      return( 
+        <SimpleGrid cols={{base:1, sm:2, md:3, lg:4}}>
+          {dapp_cards}
+        </SimpleGrid>
+      );
+    }
+    else{
+      return <DApp html_page={dapps[activeDApp]} closeFunction={handleClose} device_endpoint={deviceEndpoint}/>
+    }
+  }
+
+
+  console.log(deviceEndpoint);
+  return (
+    <>
+      {pageContent()}
+    </>
+  );
+}
+
+export default DAppContainer;
